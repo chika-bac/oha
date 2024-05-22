8e10670d27924895fc24cefa704331f3c1aa55d7
chika-bac <cocopanda.it@gmail.com>
2024-05-22 17:55:57 +0900

    ドロワーjs作成

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 /*
-スライドメニュー
+スライドメニュー: js
 ================================================ */
-const menuOpen = document.querySelector("#js-menu-open");
-const menuClose = document.querySelector("#js-menu-close");
-const menuPanel = document.querySelector("#js-menu-panel");
+const menuIcon = document.querySelector("#js-drawer-icon");
+const menuContent = document.querySelector("#js-drawer-content");
 
-const menuOptions = {
-  duration: 800,
-  easing: "ease",
-  fill: "forwards",
-};
+// エラー回避のためifが必要
+if (menuIcon) {
+  menuIcon.addEventListener("click", (e) => {
+    e.preventDefault();
+    menuIcon.classList.toggle("is-checked");
+    menuContent.classList.toggle("is-checked");
+  });
+}
 
-// メニューを開く
-menuOpen.addEventListener("click", () => {
-  menuPanel.animate({ translate: ["100vw", 0] }, menuOptions);
-});
-
-// メニューを閉じる
-menuClose.addEventListener("click", () => {
-  menuPanel.animate({ translate: [0, "100vw"] }, menuOptions);
-});
+/*
+スライドメニュー: jQuery
+================================================ */
+// $("#js-drawer-icon").on("click", function (e) {
+//   e.preventDefault();
+//   $("#js-drawer-icon").toggleClass("is-checked");
+//   $("#js-drawer-content").toggleClass("is-checked");
+// });

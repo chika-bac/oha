4967c722beaa5d90559e4d4e83ee73b7e1dfaf28
chika-bac <cocopanda.it@gmail.com>
2024-05-26 16:43:44 +0900

    スムーススクロール実装

--- js/main.js
+++ js/main.js
@@ -101,8 +101,6 @@ const io = new IntersectionObserver((entries) => {
     if (entry.isIntersecting) {
       // 要素が表示領域に入った場合にクラスを付与
       entry.target.classList.add("is-in-view");
-      // 監視を止める
-      observer.unobserve(entry.target);
     }
     // 表示領域から外れた場合
     // else {
@@ -119,3 +117,54 @@ inViewItems.forEach((inViewItem) => {
 // mapを使った書き方
 // const inViewItems = [...document.querySelectorAll(".js-in-view")];
 // inViewItems.map((inViewItem) => io.observe(inViewItem));
+
+/*
+スムーススクロール：jQuery
+================================================ */
+// href属性が#で始まるaタグをクリックした場合
+$('a[href^="#"]').on("click", function (e) {
+  // クリックした要素のhref属性を取得
+  const id = $(this).attr("href");
+  // href属性が#のみの場合はトップ（html）、それ以外はid属性のリンク先
+  const target = $(id == "#" ? "html" : id);
+  const position = $(target).offset().top;
+  $("html, body").animate(
+    {
+      scrollTop: position,
+    },
+    1000,
+    "swing" // swing or linear
+  );
+});
+
+// ドロワーメニュー内のリンククリック時にドロワーを閉じる
+$('#js-drawer-content a[href^="#"]').on("click", function (e) {
+  $("#js-drawer-icon").removeClass("is-checked");
+  $("#js-drawer-content").removeClass("is-checked");
+});
+
+/*
+Topへ戻るボタンを表示：jQuery
+================================================ */
+$(window).on("scroll", function (e) {
+  e.preventDefault();
+  // 100pxスクロールするとクラスを付与
+  if ($(window).scrollTop() > 100) {
+    $("#js-page-top").addClass("is-show");
+  } else {
+    $("#js-page-top").removeClass("is-show");
+  }
+});
+
+/*
+Topへ戻るボタンを表示：js
+================================================ */
+// const pageTop = document.querySelector("#js-page-top");
+// window.addEventListener("scroll", (e) => {
+//   e.preventDefault();
+//   if (window.scrollY > 100) {
+//     pageTop.classList.add("is-show");
+//   } else {
+//     pageTop.classList.remove("is-show");
+//   }
+// });

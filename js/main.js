990876752ded2edaf812b73ef84760bf9ce11a34
chika-bac <cocopanda.it@gmail.com>
2024-07-05 10:23:26 +0900

    微修正など

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 /*
+360pxでviewport固定
+================================================ */
+!(function () {
+  const viewport = document.querySelector('meta[name="viewport"]');
+  function switchViewport() {
+    const value = window.outerWidth > 360 ? "width=device-width,initial-scale=1" : "width=360";
+    if (viewport.getAttribute("content") !== value) {
+      viewport.setAttribute("content", value);
+    }
+  }
+  addEventListener("resize", switchViewport, false);
+  switchViewport();
+})();
+
+/*
 スライドメニュー: js
 ================================================ */
 // const menuIcon = document.querySelector("#js-drawer-icon");
